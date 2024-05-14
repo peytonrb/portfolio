3582067dce67c4356db05af7750e90c122bba61b
Peyton Bischof <peyton.bischof@hotmail.com>
2024-05-14 15:59:48 -0400

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tools from './Tools';
+import Experience from './Experience';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tools,
+  Experience,
+  Projects,
+  Skills,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
